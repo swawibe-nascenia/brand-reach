b782c6e13f6844f927f4fe24527e133a4904c71e
Tin <tinle1201@gmail.com>
2015-06-25 12:54:14 +0700

    init code

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
+# http://tech.pro/tutorial/1430/ruby-on-rails-4-authentication-with-facebook-and-omniauth
