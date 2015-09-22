c6d7c187dc657c74edfdf52f03f3899246b8e580
Naim Rajib <naim@nascenia.com>
2015-09-22 12:33:40 +0600

    Brand Explore   - Added brands controller and explore action.

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,4 @@
+class BrandsController < ApplicationController
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'brands/explore'
+
   devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks'}
   # get 'users/profile' => 'users/registrations#edit_user_profile'
 
