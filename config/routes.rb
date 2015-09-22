9b905c62ec2ee0704c2692a572374f92f543625d
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-09-22 12:52:37 +0600

    - add influencers controller where we will pull influencer related actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'influencers/index'
+
+  get 'influencer/index'
+
+  get 'influencer/index'
+
+  get 'influencer/index '
+
   devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks'}
   # get 'users/profile' => 'users/registrations#edit_user_profile'
 
