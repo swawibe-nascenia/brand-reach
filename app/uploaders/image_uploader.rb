24722683314a4a05eda640845ab87c6bb1846fec
Swawibe Alam <swawibe@bdipo.com>
2015-09-22 15:55:38 +0600

    Image tag added in Explore.html file, New version explore_image added in image uploader file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [160, 160]
   end
 
+  version :explore_image do
+    process :resize_to_fit => [nil, 120]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
