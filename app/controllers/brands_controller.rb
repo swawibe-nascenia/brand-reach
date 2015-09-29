d60338e5e87304e99f7e3b2bf17b96093a080a79
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-09-29 11:24:42 +0600

    - remove duplicate methods from brands controller after merger with explore branch

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -8,24 +8,8 @@ class BrandsController < ApplicationController
     end
   end
 
-  private
-
   def brand_user?
     current_user.user_type == 'brand' ? true : false
-  end
-
-  def new_offer
-
-  end
-
-  def send_offer
-
-  end
-
-  private
-
-  def offer_atttibutes
-      params.require(:offer).permit(:sender_id, :receiver_id, :read, :starred, :status, :message)
   end
 
   def new_offer
