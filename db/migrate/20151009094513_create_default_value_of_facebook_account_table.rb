9de5127481411311e954610b18da219ab3282290
Swawibe Alam <swawibe@bdipo.com>
2015-10-09 16:00:42 +0600

    [Campaign] : Costs showing on changing Type of Post

---
+++ db/migrate/20151009094513_create_default_value_of_facebook_account_table.rb
@@ -0,0 +1,8 @@
+class CreateDefaultValueOfFacebookAccountTable < ActiveRecord::Migration
+  def change
+    change_column :facebook_accounts, :status_update_cost, :integer, default: 0
+    change_column :facebook_accounts, :profile_photo_cost, :integer, default: 0
+    change_column :facebook_accounts, :cover_photo_cost, :integer, default: 0
+    change_column :facebook_accounts, :video_post_cost, :integer, default: 0
+  end
+end
