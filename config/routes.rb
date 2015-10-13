e48efc5e1d1b6a44268916e7e723a956ced92d2b
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-10-13 16:01:11 +0600

    - remove brands controller and make explore controller for explore action

--- config/routes.rb
+++ config/routes.rb
@@ -30,15 +30,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :brands, only: [] do
-    collection do
-      get :explore
-      post :send_offer
-    end
-    member do
-    end
-  end
-
   resources :facebook do
     collection do
       get :insights
@@ -91,5 +82,6 @@ Rails.application.routes.draw do
     end
   end
 
+  resource :explores, only: [:show]
   root to: 'public#home', as: :root
 end
