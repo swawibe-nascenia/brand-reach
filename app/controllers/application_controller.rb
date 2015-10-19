e59010b55afa0a795ac4e40367f2e0717e8a3ba7
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-10-19 10:14:04 +0600

    - skip check_profile_completion befor_filter for device controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate_user!, :initialize_server_subscription, :check_profile_completion
   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :check_profile_completion, if: :devise_controller?
 
   layout :layout_by_resource
 
