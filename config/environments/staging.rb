7cee75287f9887ed79a7ef0bc816a211e92f5163
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-10-20 13:57:27 +0600

    - add smtp configuration to staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,6 +14,25 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  # setup gmail smtp for this environment
+  config.action_mailer.default_url_options = { host: 'localhost', port: 80 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'my_app.com',
+      user_name:            'hasanuzzaman@nascenia.com',
+      password:             '284sumon',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
