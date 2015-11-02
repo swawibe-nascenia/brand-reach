d6f2898d705fed07706d30494e5fd32c305e0f91
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-11-02 09:58:46 +0600

    - handle http request by bypassing to session super

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,21 @@ class SessionsController < Devise::SessionsController
   respond_to :html, :json, :js
 
   def create
-    self.resource = warden.authenticate(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
-    @login_success = true
-
-    if resource
-      Rails.logger.info "------------------- current sign in success for #{ self.resource.inspect }"
-      sign_in(resource_name, resource)
+    if request.format.symbol == :html
+      super
     else
-      Rails.logger.info "------------------- current sign in fail for #{ self.resource.inspect }"
-      @login_success = false
-    end
+      self.resource = warden.authenticate(auth_options)
+      set_flash_message(:notice, :signed_in) if is_navigational_format?
+      @login_success = true
 
+      if resource
+        Rails.logger.info "------------------- current sign in success for #{ self.resource.inspect }"
+        sign_in(resource_name, resource)
+      else
+        Rails.logger.info "------------------- current sign in fail for #{ self.resource.inspect }"
+        @login_success = false
+      end
+
+    end
   end
 end
