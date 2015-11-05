a451a9387116f528a9c2a0e97c17de0eddb3b6f6
Swawibe Alam <swawibe@bdipo.com>
2015-11-05 16:23:33 +0600

    [Validation]:   Landmark Removed from validation field

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,10 @@ class User < ActiveRecord::Base
 
   enum gender: [:male, :female, :other]
 
-  BRAND_PROFILE_COMPLETENESS = [:company_name, :company_email, :industry, :phone, :street_address, :landmark,
+  BRAND_PROFILE_COMPLETENESS = [:company_name, :company_email, :industry, :phone, :street_address,
                           :city, :state, :country, :zip_code, :short_bio, :first_name, :last_name]
 
-  INFLUENCER_PROFILE_COMPLETENESS = [:industry, :phone, :street_address, :landmark,
+  INFLUENCER_PROFILE_COMPLETENESS = [:industry, :phone, :street_address,
                           :city, :state, :country, :zip_code, :short_bio, :first_name, :last_name]
 
   FACEBOOK_ACCOUNT_COMPLETENESS = [:status_update_cost, :profile_photo_cost, :cover_photo_cost, :video_post_cost]
