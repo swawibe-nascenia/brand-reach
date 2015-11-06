fc8268d642164a534f60ed40d9b91db17acaf3ee
Swawibe Alam <swawibe@bdipo.com>
2015-11-06 12:29:41 +0600

    [Bug Fixed]:  1. Browsers cache invalidated so after sign out back button will redirect to sign in page, 2. Iphone viewport problem solved, so now iphone will be responsive, 3. Homepage 3rd section Icons alignment problem fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :authenticate_user!, :initialize_server_subscription, :check_profile_completion
+  before_filter :authenticate_user!, :initialize_server_subscription, :check_profile_completion, :set_cache_buster
   before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :check_profile_completion, if: :devise_controller?
 
   layout :layout_by_resource
+
+  def set_cache_buster
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
+  end
 
   def after_sign_in_path_for(resource)
     if current_user.influencer? && current_user.sign_in_count <= 1
