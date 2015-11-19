ca542ee2ba8ced528a549d2e8d5012d87248690c
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-11-19 18:27:46 +0600

    explore page design solving on the way

--- app/controllers/explores_controller.rb
+++ app/controllers/explores_controller.rb
@@ -1,19 +1,25 @@
 class ExploresController < ApplicationController
   before_filter :is_brand?, only: [:show]
 
+  #  Range.new(*'1..10'.split('..').map(&:to_i))
   def show
     @influencers = User.influencers.where(profile_complete: true)
 
     if params[:search_key].present?
       wildcard_search = "%#{params[:search_key]}%"
       @influencers = @influencers.where('industry LIKE :search OR country_name LIKE :search OR state_name LIKE :search', search: wildcard_search)
-    elsif params[:advance_search].present?
-      @influencers = @influencers.where(industry: params[:category]) if params[:category].present?
-      @influencers = @influencers.where(country: params[:country]) if params[:country].present?
-      #TODO @influencers = @influencers.where(fb_average_cost: [params['followers-range-value-lower']..params['followers-range-value-upper']])
-      @influencers = @influencers.where(fb_average_cost: [params['influencer-price-value-lower']..params['influencer-price-value-upper']])
     end
-    
+    @influencers = @influencers.where(industry: params[:category]) if params[:category].present?
+    @influencers = @influencers.where(country: params[:country]) if params[:country].present?
+    if params[:price].present?
+      price_range = Range.new(*params[:price].split('..').map(&:to_i))
+      @influencers = @influencers.where(fb_average_cost: price_range)
+    end
+    if params[:followers].present?
+      price_range = Range.new(*params[:followers].split('..').map(&:to_i))
+      @influencers = @influencers.where(fb_average_cost: price_range)
+    end
+
     Rails.logger.info @influencers.inspect
     @influencers = @influencers.order(:name).page params[:page]
     @influencers
