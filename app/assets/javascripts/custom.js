e5b25bec0d6a9716138ebb05fde5e1c4b398efc3
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-11-24 10:44:49 +0600

    add handlebar helper for string truncation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,6 +3,19 @@
  */
 $(function(){
 
+//    ======================== handlebar helper method add ======================
+    Handlebars.registerHelper ('truncate', function (str, len) {
+        if (str && str.length > len && str.length > 0) {
+            var new_str = str + " ";
+            new_str = str.substr (0, len);
+            new_str = str.substr (0, new_str.lastIndexOf(" "));
+            new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+            return new Handlebars.SafeString ( new_str +'...' );
+        }
+        return str;
+    });
+
 //    toggle star of selected offers
     $('.make-all-stared').click(function(){
         var selected_offer_ids = seleted_offer_ids();
