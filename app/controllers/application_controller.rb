df8636e7c561fd62d4e3ff6c9ee47a9185d3e1e7
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-11-30 17:19:01 +0600

    redirect influencer to facebook insights page after login if profile is completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if current_user.influencer? && current_user.profile_complete?
-      insights_facebooks_path
+    if current_user.influencer?
+      insights_facebook_index_path
     else
       profile_profile_index_path
     end
