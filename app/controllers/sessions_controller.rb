5ca8871df648eb67428b6af81024fb48cf4186c1
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-12-08 19:18:10 +0600

    admin log in page design complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,42 +10,48 @@ class SessionsController < Devise::SessionsController
     Rails.logger.info "------------------- temporary user by email #{ temp_user.inspect }"
 
     if temp_user
-      if temp_user.verified?
-        #   verified user by brandreach
-        user = warden.authenticate(auth_options)
+      if !(params[:admin].present? && (temp_user.admin? || temp_user.super_admin?))
+        Rails.logger.info "No authority "
+        flash[:error] = 'You are unauthorized to access the page.'
+        redirect_to admin_path
+      else
+        if temp_user.verified?
+          #   verified user by brandreach
+          user = warden.authenticate(auth_options)
 
-        if user
-          # authentication success
-          Rails.logger.info "------------------- current sign in success for #{ user.inspect }"
-          @login_success = true
-          sign_in(resource_name, user)
+          if user
+            # authentication success
+            Rails.logger.info "------------------- current sign in success for #{ user.inspect }"
+            @login_success = true
+            sign_in(resource_name, user)
 
-          unless user.is_active?
-            user.update_attribute(:is_active, true)
-          end
+            unless user.is_active?
+              user.update_attribute(:is_active, true)
+            end
 
-          respond_to do |format|
-            format.js { }
-            format.html { redirect_to after_sign_in_path_for(resource) }
-          end
-        else
-          #   authentication fail
-          Rails.logger.info "------------------- current sign in authentication fail for #{ user.inspect }"
-          if params[:admin].present?
-            flash[:error] = 'Email, password not matching'
-             redirect_to admin_path
-          else
             respond_to do |format|
-              format.js { @messages << 'Email, password not matching' }
-              format.html { flash.now[:error] = 'Email, password not matching' }
+              format.js { }
+              format.html { redirect_to after_sign_in_path_for(resource) }
+            end
+          else
+            #   authentication fail
+            Rails.logger.info "------------------- current sign in authentication fail for #{ user.inspect }"
+            if params[:admin].present?
+              flash[:error] = 'Email, password not matching'
+              redirect_to admin_path
+            else
+              respond_to do |format|
+                format.js { @messages << 'Email, password not matching' }
+                format.html { flash.now[:error] = 'Email, password not matching' }
+              end
             end
           end
-        end
-      else
-        #   un verified user
-        respond_to do |format|
-          format.js { @messages << 'Your account is not active yet' }
-          format.html { flash.now[:error] = 'Your account is not active yet' }
+        else
+          #   un verified user
+          respond_to do |format|
+            format.js { @messages << 'Your account is not active yet' }
+            format.html { flash.now[:error] = 'Your account is not active yet' }
+          end
         end
       end
     else
