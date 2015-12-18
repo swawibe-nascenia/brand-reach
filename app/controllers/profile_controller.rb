1ae68069aeda66ea5299e2908cf25680fb635313
Swawibe Alam <swawibe@bdipo.com>
2015-12-18 13:02:57 +0600

    Multiple Industries Nil Nilclass issue solved

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -18,6 +18,8 @@ class ProfileController < ApplicationController
 
   def update
     @industries = Category.all.order(:name).pluck(:name)
+    @all_industries = Category.all.order(:name)
+    @selected_industries = @user.categories
     if @user.update(user_params.except(:current_password, :password, :password_confirmation, :industry))
       if user_params[:current_password].present? && user_params[:password].present?
         if @user.update_with_password(user_params.except(:facebook))
