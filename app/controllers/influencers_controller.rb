89882fd4740e44ce3bd0a5901317113195a2ca18
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2015-12-21 17:26:20 +0600

    change influencer search page design

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -11,7 +11,7 @@ class InfluencersController < ApplicationController
   def search
     wildcard_search = "%#{params[:search_key].strip! || params[:search_key]}%"
     all_offers_key = current_user.campaigns_received.where.not(deleted_by_influencer: true).pluck(:id)
-    @messages = Message.where(campaign_id: all_offers_key).where('body LIKE :search', search: wildcard_search)
+    @messages = Message.where(campaign_id: all_offers_key).where('body LIKE :search', search: wildcard_search).includes(:campaign)
     #TODO @influencers = @influencers.where('industry LIKE :search OR country_name LIKE :search OR state_name LIKE :search', search: wildcard_search)
   end
 
