9b115314fceeb59a10312e1844595cfe4f6ee1dd
Rafi Adnan <rafi@nascenia.com>
2015-12-30 13:15:27 +0600

    move code into method

--- db/migrate/20151228071017_add_photo_post_cost_to_facebook_accounts.rb
+++ db/migrate/20151228071017_add_photo_post_cost_to_facebook_accounts.rb
@@ -1,7 +1,6 @@
 class AddPhotoPostCostToFacebookAccounts < ActiveRecord::Migration
   def change
     add_column :facebook_accounts, :photo_post_cost, :integer
+    FacebookAccount.all.update_all(:photo_post_cost => 10)
   end
-
-  FacebookAccount.all.update_all(:photo_post_cost => 10)
 end
