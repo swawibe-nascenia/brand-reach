f221230a5969cacaf2969a1f3f6da50ad1bef21e
Rafi Adnan <rafi@nascenia.com>
2016-01-04 15:18:30 +0600

    remove unneeded legacy insight fetching methods

--- app/services/insight_service.rb
+++ app/services/insight_service.rb
@@ -162,63 +162,6 @@ class InsightService < BaseService
     end
   end
 
-  def get_info(page_id = 'VoHoaiLinh')
-    response = nil
-    begin
-      Rails.logger.info @graph.access_token
-      response = @graph.get_object(page_id)
-      name = page_id
-      if response["about"] && response["about"].length > 0
-        name = response["about"].to_s[0..100]
-      end
-      Page.create({
-                      username: page_id,
-                      name: name
-                  })
-    rescue Exception => e
-      Rails.logger.error("!!!!ERROR: #{e.inspect}")
-      @is_error = true
-    end
-    response
-  end
-
-  def get_posts(page_id = 'VoHoaiLinh')
-    response = nil
-    begin
-      Rails.logger.info @graph.access_token
-      response = @graph.get_object("#{page_id}/posts")
-    rescue Exception => e
-      Rails.logger.error("!!!!ERROR: #{e.inspect}")
-      @is_error = true
-    end
-    response
-  end
-
-  def get_insights(page_id = 'VoHoaiLinh')
-    response = nil
-    begin
-      @pages = @graph.get_object('me/accounts')
-      token = @current_user.access_token
-      Rails.logger.error("user------------------------------------------------")
-      Rails.logger.error(token)
-      params = {pretty: 0, since: 1.months.ago.to_i, suppress_http_code: 1, until: Time.now.to_i}
-      @pages.each do |pg|
-        Rails.logger.error("pageid==page_id: #{pg['id'] || pg['name']} === #{page_id}")
-        if pg['id']==page_id.downcase || pg['name'].to_s.downcase == page_id.downcase
-          token = pg['access_token']
-        end
-      end
-      Rails.logger.error("page------------------------------------------------")
-      Rails.logger.error(token)
-      grapher = Koala::Facebook::API.new token
-      response = grapher.get_object("#{page_id}/insights", params)
-    rescue Exception => e
-      Rails.logger.error("!!!!ERROR: #{e.inspect}")
-      @is_error = true
-    end
-    response
-  end
-
   private
 
   def get_aggregate_by_region(id, metric, region)
