0d1551c740d8f0fb0579accae26526ade6a1d551
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2016-01-08 15:23:30 +0600

    change all brand button stylesheet

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -169,11 +169,11 @@ class CampaignsController < ApplicationController
 
   def brand_campaign
     @campaigns = Campaign.engaged_campaigns_from(current_user)
-
-    if @campaigns.blank?
-      flash[:error] = 'You have no campaign'
-      return redirect_to explores_path
-    end
+    #
+    # if @campaigns.blank?
+    #   flash[:error] = 'You have no campaign'
+    #   return redirect_to explores_path
+    # end
 
     @campaign = @campaigns.find_by_id(params[:id]) if params[:id].present?
     @campaign = @campaigns.last if @campaign.nil?
