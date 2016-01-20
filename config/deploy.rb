8d5666230a166c1e1c46a2361df8fa8291aa20f7
hasanuzzaman sumon <hasanuzzaman@nascenia.com>
2016-01-20 11:44:54 +0600

    add wehenever require file to capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-require 'whenever/capistrano'
 
 set :application, 'brand-reach'
 set :repo_url, 'git@github.com:rafi-nascenia/brand-reach.git'
