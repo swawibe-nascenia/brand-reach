4b5b8d7b720ff65cbf485a54016534c68adefbff
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2016-02-16 15:22:49 +0600

    brand's explore now return account instead influencer

--- app/controllers/explores_controller.rb
+++ app/controllers/explores_controller.rb
@@ -35,19 +35,24 @@ class ExploresController < ApplicationController
     @influencers = @influencers.where(state: params[:state]) if params[:state].present?
     @influencers = @influencers.where(country: params[:country]) if params[:country].present?
 
-    if params[:price].present?
-      price_range = Range.new(*params[:price].split('..').map(&:to_i))
-      @influencers = @influencers.where(fb_average_cost: price_range)
-    end
+    # if params[:price].present?
+    #   price_range = Range.new(*params[:price].split('..').map(&:to_i))
+    #   @influencers = @influencers.where(fb_average_cost: price_range)
+    # end
+    #
+    # if params[:followers].present?
+    #   followers = Range.new(*params[:followers].split('..').map(&:to_i))
+    #   @influencers = @influencers.where(max_followers: followers)
+    # end
+    #
+    #
+    # @influencers = @influencers.order(:name).page(params[:page]).per(8)
+    #
+    # @influencers
 
-    if params[:followers].present?
-      followers = Range.new(*params[:followers].split('..').map(&:to_i))
-      @influencers = @influencers.where(max_followers: followers)
-    end
-
-    Rails.logger.info @influencers.inspect
-    @influencers = @influencers.order(:name).page(params[:page]).per(8)
-    @influencers
+    # send only selected influence's account
+    @accounts = FacebookAccount.where(influencer_id: @influencers.pluck(:id))
+                               .page(params[:page]).per(8)
   end
 
   private
