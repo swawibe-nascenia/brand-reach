b8857a465e832a6ff220a4fe096f655de3df7fa7
Swawibe Ul Alam <swawibe@bdipo.com>
2016-03-17 14:45:27 +0600

    - Production Email address added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { host: 'thebrandreach.com', port: 80 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'my_app.com',
+      user_name:            'hasanuzzaman@nascenia.com',
+      password:             '284sumon',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
